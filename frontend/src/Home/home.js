7d0f8d09e5e6485cb7964b4d884ce73a67d88754
Pranay-Erra <pranayerra2003@gmail.com>
2025-01-30 15:13:33 +0530

    final code

--- frontend/src/Home/home.js
+++ frontend/src/Home/home.js
@@ -1,35 +1,30 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import './home.css';  
+import './home.css';
+
 const Home = () => {
     return (
         <div className="home-container">
-            <h1>Welcome to My Web App</h1>
-            <p>Select a feature from the list below:</p>
-            <div className="features-container">
-                <div className="feature-card">
-                    <h3>Name</h3>
-                    <Link to="/name">Go to Name</Link>
+            <h1 className="home-title">Welcome to My Web App</h1>
+            <p className="home-description">Select a feature from the list below:</p>
+            <div className="features-grid">
+                <div className="feature-item">
+                    <Link to="/name">Name</Link>
                 </div>
-                <div className="feature-card">
-                    <h3>Clock</h3>
-                    <Link to="/time">Go to Clock</Link>
+                <div className="feature-item">
+                    <Link to="/time">Clock</Link>
                 </div>
-                <div className="feature-card">
-                    <h3>Star Pattern 1</h3>
-                    <Link to="/p1">Go to Pattern 1</Link>
+                <div className="feature-item">
+                    <Link to="/p1">Star Pattern 1</Link>
                 </div>
-                <div className="feature-card">
-                    <h3>Star Pattern 2</h3>
-                    <Link to="/p2">Go to Pattern 2</Link>
+                <div className="feature-item">
+                    <Link to="/p2">Star Pattern 2</Link>
                 </div>
-                <div className="feature-card">
-                    <h3>Counter</h3>
-                    <Link to="/counter">Go to Counter</Link>
+                <div className="feature-item">
+                    <Link to="/counter">Counter</Link>
                 </div>
-                <div className="feature-card">
-                    <h3>Temperature Converter</h3>
-                    <Link to="/temp">Go to Converter</Link>
+                <div className="feature-item">
+                    <Link to="/temp">Temperature Converter</Link>
                 </div>
             </div>
         </div>
