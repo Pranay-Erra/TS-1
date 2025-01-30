629faf295b8056cc26a1d29c1261cd172ea7ba8b
Pranay-Erra <pranayerra2003@gmail.com>
2025-01-30 14:54:29 +0530

    appiled css to home page

--- frontend/src/Home/home.js
+++ frontend/src/Home/home.js
@@ -1,19 +1,37 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-
+import './home.css';  
 const Home = () => {
     return (
-        <div style={{ textAlign: 'center', padding: '20px' }}>
+        <div className="home-container">
             <h1>Welcome to My Web App</h1>
             <p>Select a feature from the list below:</p>
-            <ul>
-                <li><Link to="/name">Name</Link></li>
-                <li><Link to="/time">Clock</Link></li>
-                <li><Link to="/p1">Star Pattern 1</Link></li>
-                <li><Link to="/p2">Star Pattern 2</Link></li>
-                <li><Link to="/counter">Counter</Link></li>
-                <li><Link to="/temp">Temperature Converter</Link></li>
-            </ul>
+            <div className="features-container">
+                <div className="feature-card">
+                    <h3>Name</h3>
+                    <Link to="/name">Go to Name</Link>
+                </div>
+                <div className="feature-card">
+                    <h3>Clock</h3>
+                    <Link to="/time">Go to Clock</Link>
+                </div>
+                <div className="feature-card">
+                    <h3>Star Pattern 1</h3>
+                    <Link to="/p1">Go to Pattern 1</Link>
+                </div>
+                <div className="feature-card">
+                    <h3>Star Pattern 2</h3>
+                    <Link to="/p2">Go to Pattern 2</Link>
+                </div>
+                <div className="feature-card">
+                    <h3>Counter</h3>
+                    <Link to="/counter">Go to Counter</Link>
+                </div>
+                <div className="feature-card">
+                    <h3>Temperature Converter</h3>
+                    <Link to="/temp">Go to Converter</Link>
+                </div>
+            </div>
         </div>
     );
 };
