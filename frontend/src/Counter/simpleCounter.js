e9b1e19a9a09074aa4a9c3bd5ba6060a554385f3
Pranay-Erra <pranayerra2003@gmail.com>
2025-01-30 14:42:48 +0530

    added css to counter

--- frontend/src/Counter/simpleCounter.js
+++ frontend/src/Counter/simpleCounter.js
@@ -1,33 +1,43 @@
 import React, { useState } from "react";
 import "./simpleCounter.css";
+
 const Counter = () => {
   const [count, setCount] = useState(0);
 
-  const Increment=()=>{
-    setCount(count + 1)
-  }
+  const Increment = () => {
+    setCount(count + 1);
+  };
 
-  const Decrement=()=>{
-    setCount(count - 1)
-  }
+  const Decrement = () => {
+    setCount(count - 1);
+  };
 
-  const Reset=()=>{
-    setCount(0)
-  }
+  const Reset = () => {
+    setCount(0);
+  };
+
   const getColor = () => {
     return count > 0 ? "blue" : count < 0 ? "red" : "black";
   };
 
   return (
-    <div>
-      <h2 style={{ color: getColor() }}>{count}</h2>
-      <button onClick={Increment} >Increment</button>
-      <button onClick={Decrement} >Decrement</button>
-      <button onClick={Reset} >Reset</button>
+    <div className="counter-container">
+      <h2 className="counter-value" style={{ color: getColor() }}>
+        {count}
+      </h2>
+      <div className="button-container">
+        <button className="counter-button increment" onClick={Increment}>
+          Increment
+        </button>
+        <button className="counter-button decrement" onClick={Decrement}>
+          Decrement
+        </button>
+        <button className="counter-button reset" onClick={Reset}>
+          Reset
+        </button>
+      </div>
     </div>
   );
 };
-
-
 
 export default Counter;
